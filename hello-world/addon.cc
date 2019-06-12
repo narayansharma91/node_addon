e08bfff7e9ccf0b2a750f9bc065193e8d5442fad
Narayan Sharma <narayansharma@ucreate.co.in>
2019-06-12 13:04:17 +0530

    Performance comparision using buble sort between JavaScript and C++

--- hello-world/addon.cc
+++ hello-world/addon.cc
@@ -4,11 +4,39 @@
 using namespace v8;
 using namespace std;
 
-void helloWorld(const FunctionCallbackInfo<Value>& args) {
+void helloWorld(const FunctionCallbackInfo<Value> &args)
+{
    cout << "Hello, world!" << endl;
 }
 
-void Initialize(Local<Object> exports) {
+void bubbleSort(const FunctionCallbackInfo<v8::Value> &args)
+{
+   Isolate *isolate = args.GetIsolate();
+   double result = 0;
+   std::vector<int> arr;
+   int i, j;
+   int length = 50000;
+   for (i = length; i > 0; i--)
+   {
+      arr.push_back(i);
+   }
+   for (i = 0; i < length; i++)
+   {
+      for (j = 0; j <= length - i - 1; j++)
+      {
+         if (arr[j] > arr[j + 1])
+         {
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+         }
+      }
+   }
+}
+
+void Initialize(Local<Object> exports)
+{
+   NODE_SET_METHOD(exports, "bubbleSort", bubbleSort);
    NODE_SET_METHOD(exports, "getSomethingFromAddOn", helloWorld);
 }
 
--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,28 @@
-const { getSomethingFromAddOn } = require('./build/Release/addon')
-getSomethingFromAddOn();//hello world
+const { bubbleSort } = require('./build/Release/addon')
+console.time('c++ time');
+bubbleSort()
+console.timeEnd('c++ time')
+
+console.time('JavaScript time');
+bubbleSortJs();
+console.timeEnd('JavaScripttime');
+
+function bubbleSortJs() {
+    const length = 50000;
+    const arr = [];
+    for (i = length; i > 0; i--) {
+        arr.push(i);
+    }
+    for (i = 0; i < length; i++) {
+        for (j = 0; j <= length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 
 
